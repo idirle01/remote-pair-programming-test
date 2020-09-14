7c8c6632689c2c87acda5f808c2e74df8b4d5547
phil.hardwick <philipchardwick@gmail.com>
2020-09-14 09:11:07 +0100

    Add test to check the spring context starts

---
+++ src/test/java/com/eeveebank/simplespring/CarApplicationTest.java
@@ -0,0 +1,19 @@
+package com.eeveebank.simplespring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ActiveProfiles("test")
+public class CarApplicationTest {
+
+    @Test
+    public void contextStarts() {
+    }
+}
